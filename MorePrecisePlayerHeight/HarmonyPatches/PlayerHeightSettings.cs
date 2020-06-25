fcfe9692afd2210c5967e7609c0e2ac6bfd4d4dc
Eris <erisapps@outlook.be>
2020-06-26 01:47:52 +0200

    Wrote the entire plugin 🚀

---
+++ MorePrecisePlayerHeight/HarmonyPatches/PlayerHeightSettings.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using TMPro;
+
+namespace MorePrecisePlayerHeight.HarmonyPatches
+{
+	[HarmonyPatch(typeof(PlayerHeightSettingsController))]
+	[HarmonyPatch("RefreshUI", MethodType.Normal)]
+	class PlayerHeightSettings
+	{
+		static void Postfix(ref TextMeshProUGUI ____text, ref PlayerSpecificSettings ____playerSettings)
+		{
+			Plugin.Logger.Info($"{nameof(MorePrecisePlayerHeight)} postfix called" );
+
+			____text.text = $"{(object) ____playerSettings.playerHeight:0.00}m";
+		}
+	}
+}
---
+++ MorePrecisePlayerHeight/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using IPA.Logging;
+
+namespace MorePrecisePlayerHeight
+{
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin
+	{
+		private const string _harmonyId = "be.erisapps.morepreciseplayerheight"; 
+		
+		public static Harmony HarmonyInstance;
+		public static Logger Logger;
+
+		[Init]
+		public void Init(IPA.Logging.Logger logger)
+		{
+			Logger = logger;
+		}
+
+		[OnEnable]
+		public void OnStart()
+		{
+			Logger.Log(Logger.Level.Info, $"{nameof(MorePrecisePlayerHeight)} enabled" );
+			HarmonyInstance = new Harmony(_harmonyId);
+			HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[OnDisable]
+		public void OnDisable()
+		{
+			Logger.Log(Logger.Level.Info, $"{nameof(MorePrecisePlayerHeight)} disabled" );
+
+			HarmonyInstance.UnpatchAll(_harmonyId);
+		}
+	}
+}
